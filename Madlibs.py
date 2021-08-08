39a0b8d48a2fe623560f63cf84945468ae3ed044
Katalt <59557739+Katalt@users.noreply.github.com>
2021-08-08 15:21:13 +0400

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,9 @@
+# Madlibs
+
+color = input("Enter the color: ")
+plural_noun = input("Enter the plural noun: ")
+celebrity = input("Enter the Celebrity: ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
